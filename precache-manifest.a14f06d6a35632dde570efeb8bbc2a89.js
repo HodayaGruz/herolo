16a845b6d7d4cce896744b9ffb1d4de4a3057ca8
hodaya <hodayag@welldone-software.com>
2018-10-26 12:26:56 +0300

    Updates

---
+++ precache-manifest.a14f06d6a35632dde570efeb8bbc2a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "1cfd41f6fa6754456c2d",
+    "url": "/myapp/static/js/main.1cfd41f6.chunk.js"
+  },
+  {
+    "revision": "9b3b2e6698b19153bb59",
+    "url": "/myapp/static/js/1.9b3b2e66.chunk.js"
+  },
+  {
+    "revision": "1cfd41f6fa6754456c2d",
+    "url": "/myapp/static/css/main.510f17a5.chunk.css"
+  },
+  {
+    "revision": "7333893dea587abf7a7ca2f1b3ab1df9",
+    "url": "/myapp/index.html"
+  }
+];
